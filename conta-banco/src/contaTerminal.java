ccc5f9e4f00023fcda23b8282f777f9ab4dd6d45
David-Alexandre-Lima <davidalexandrelima@gmail.com>
2024-06-25 14:38:19 -0300

    add regra de negócio

--- conta-banco/src/contaTerminal.java
+++ conta-banco/src/contaTerminal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bem vindo ao Banco");
+        System.out.println("Insira o nome");
+        String nome = sc.nextLine();
+        System.out.println("Insira o número da agência");
+        String agencia = sc.nextLine();
+        System.out.println("Infome o número");
+        int numero = sc.nextInt();
+        System.out.println("Insira o saldo");
+        double saldo = sc.nextDouble();
+        System.out.println("Insira o saldo");
+        System.out.println("Parabéns, " + nome + " seu cadastro está completo. Agência: " + agencia + " numero: "
+                + numero + " saldo: " + saldo);
     }
 }
